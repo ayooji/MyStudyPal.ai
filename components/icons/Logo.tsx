9ea2e2762125609a14feb91fc7f4895c989378d8
ayo <oji@applogicx.com>
2022-09-13 01:02:43 -0600

    commit

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,15 +1,9 @@
-import React from 'react';
-
 const Logo = ({ className = '', ...props }) => (
-  <img
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
-    src="/logo.jpg"
-    alt="Logo"
-    className={className}
-    {...props}
-  />
-);
 
-export default Logo;
+  <div>
+     <img src="logo.jpg" height="34px" width="35px" style={{verticalAlign: 'bottom'}} />
+  </div>
+     
+  );
+  
+  export default Logo;
