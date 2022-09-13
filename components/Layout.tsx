0544e863a6e92b48f327d260147dfdb23fded977
ayo <oji@applogicx.com>
2022-09-12 18:14:42 -0600

    commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 
+
 import Navbar from 'components/ui/Navbar';
 import Footer from 'components/ui/Footer';
 import { ReactNode } from 'react';
@@ -14,11 +15,20 @@ interface Props {
 export default function Layout({ children, meta: pageMeta }: Props) {
   const router = useRouter();
   const meta = {
-    title: 'Next.js Subscription Starter',
-    description: 'Brought to you by Vercel, Stripe, and Supabase.',
-    cardImage: '/og.png',
+    title: 'Mystudypal.ai',
+    description: 'The best study AI',
+    canonical: 'https://mystudypal.ai',
+    meta:{
+      charset: 'UTF-8',
+      name: {
+        keywords: 'Study, AI, mystudypal '
+       },
+     },
+    cardImage: '/logo.jpg',
+
     ...pageMeta
   };
+
 
   return (
     <>
@@ -27,14 +37,14 @@ export default function Layout({ children, meta: pageMeta }: Props) {
         <meta name="robots" content="follow, index" />
         <link href="/favicon.ico" rel="shortcut icon" />
         <meta content={meta.description} name="description" />
-        <meta property="og:url" content={`https://subscription-starter.vercel.app${router.asPath}`} />
+        <meta property="og:url" content={`https://my-study-pal-ai.vercel.app/`} />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.cardImage} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@vercel" />
+        <meta name="twitter:site" content="@mystudypal.ai" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.cardImage} />
