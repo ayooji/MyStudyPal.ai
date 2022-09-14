5b539ca3aa671b0416574f207919bafd511c18da
ayo <oji@applogicx.com>
2022-09-14 02:41:49 -0600

    commit

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,8 +3,37 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
+import styled from 'styled-components'
 
 
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  label {
+    text-align: center;
+    margin-bottom: 16px;
+  }
+
+  input {
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    width: 300px;
+  }
+
+  input[type='submit'] {
+    background-color: #4caf50;
+    color: white;
+    padding: 8px;
+    border: none;
+    border-radius: 4px;
+    width: 300px;
+    cursor: pointer;
+  }
+`
 
 const user = {
   name: 'Tom Cook',
@@ -192,9 +221,16 @@ export default function Example() {
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             {/* Replace with your content */}
             <div className="px-4 py-6 sm:px-0">
+            <Form>
+                    <label>
+                        Type your question or problem:
+                        <input type="text" />
+                    </label>
+                    <input type="submit" value="Submit" />
+                </Form>
+            </div>
               <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
               
-            </div>
             {/* /End replace */}
           </div>
         </main>
