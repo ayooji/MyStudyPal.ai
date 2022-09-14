4948a5f80ec1c1147d88259ae0db95274ff33f60
ayo <oji@applogicx.com>
2022-09-14 00:11:57 -0600

    commit

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -12,7 +12,7 @@ interface Props {
   products: ProductWithPrice[];
 }
 
-type BillingInterval = 'year' | 'month';
+type BillingInterval = 'year' | 'month' | 'week';
 
 export default function Pricing({ products }: Props) {
   const router = useRouter();
@@ -78,6 +78,17 @@ export default function Pricing({ products }: Props) {
             plans unlock additional features.
           </p>
           <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
+          <button
+              onClick={() => setBillingInterval('week')}
+              type="button"
+              className={`${
+                billingInterval === 'week'
+                  ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
+                  : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
+              } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
+            >
+              Weekly billing
+            </button>
             <button
               onClick={() => setBillingInterval('month')}
               type="button"
@@ -89,6 +100,7 @@ export default function Pricing({ products }: Props) {
             >
               Monthly billing
             </button>
+            
             <button
               onClick={() => setBillingInterval('year')}
               type="button"
