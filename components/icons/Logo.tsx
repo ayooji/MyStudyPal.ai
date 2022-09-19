260181ddbc5225c47373f9b96159c827219a098e
ayo <oji@applogicx.com>
2022-09-19 01:44:26 -0600

    commit

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -1,9 +1,20 @@
-const Logo = ({ className = '', ...props }) => (
+import * as React from "react";
 
-  <div>
+
+interface Props {
+  viewBox: string;
+  alt: string;
+  crossOrigin?: "anonymous" | "use-credentials";
+  decoding?: "async" | "auto" | "sync";
+  height: string | number;
+  loading?: "lazy" | "eager";
+  src: string;
+  className:string;
+}
+
+const Logo: React.FC<Props> = ({ viewBox, className = '', ...props }) => (
+   <div>
      <img src="logo.jpg" height="34px" width="35px" style={{verticalAlign: 'bottom'}} />
   </div>
-     
-  );
-  
-  export default Logo;
+)
+export default Logo;
