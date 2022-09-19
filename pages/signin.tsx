692a7bc2b9562b8196ee4621cd9c5e32ff5e02f9
ayo <oji@applogicx.com>
2022-09-19 02:19:55 -0600

    commit

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -66,7 +66,7 @@ const SignIn = () => {
       <div className="flex justify-center height-screen-helper">
         <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
           <div className="flex justify-center pb-12 ">
-            <Logo width="64px" height="64px" />
+            <Logo alt={''} height={''} src={''} className={''}/>
           </div>
           <div className="flex flex-col space-y-4">
             {message.content && (
